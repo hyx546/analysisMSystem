2fc88edcd4ff165396247819de5ae2ab8c2e99dc
hanyuxi <yuxihan@whale.im>
2021-01-14 21:32:07 +0800

    [be-demo]:爬取接口数据存入数据库

--- analyst/server/src/demo.ts
+++ analyst/server/src/demo.ts
@@ -7,42 +7,54 @@ mongooseData()
 const request = require("request");//request请求库
 let data = [] // 把这个数组写入excel   
 request({
-  url: "http://47.105.132.73:8011/server/project/list?webMonitorId=2",//你要请求的地址
-  method: "get",//请求方法 post get
+  url: "https://backsite.hubing.online/Monitor/PageTopStatis",//你要请求的地址
+  method: "POST",//请求方法 post get
   json: true,
   headers: {
-    "content-type": "application/json",
-    "Cookie": ""//如果携带了cookie
+    "content-type": "application/json,text/plain",
+    // "Accept-Encoding": "gzip, deflate, br",
+    // "Accept": "*/*",
+    // "Cookie": "",// 如果携带了cookie
+    // "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Headers": "*"
   },
-  // body: { "timeType": 0, "webMonitorId": "2" },//这里是post 传的参数 如果是get 方法在url上拼接就好了
+  body: JSON.stringify({
+    TimeQuantum: 4,
+    appKey: '5ea9a55e5b0dd76c634bed78',
+    eTime: '',
+    sTime: '',
+    top: 8
+  }),//这里是post 传的参数 如果是get 方法在url上拼接就好了
 }, function (error, response, body) {
   console.log('-----', response.statusCode);
 
   if (!error && response.statusCode == 200) {
-      console.log(body)
-      const rows = body.data
-      let title = ['id','webMonitorId','projectName','projectType','userTag']//设置表头
-      data.push(title) // 添加完表头 下面就是添加真正的内容了
-      rows.forEach((e) => {
-          let arrInner = []
-          arrInner.push(e.id)
-          arrInner.push(e.webMonitorId)
-          arrInner.push(e.projectName)
-          arrInner.push(e.projectType)
-          arrInner.push(e.userTag);
-          // Create(Test, {
-          //   id: e.id,
-          //   webMonitorId: e.webMonitorId,
-          //   projectName: e.projectName,
-          //   projectType: e.projectType,
-          //   userTag:e.userTag
-          // }, (a) => {
-          //   console.log('-----a',a);
-            
-          // })
-          data.push(arrInner)
-      });
-      console.log('----data',data);
+    console.log(body)
+    const rows = body.Data;
+    console.log('-----rows',rows);
+    
+    let title = ['id', 'webMonitorId', 'projectName', 'projectType', 'userTag']//设置表头
+    data.push(title) // 添加完表头 下面就是添加真正的内容了
+    rows.forEach((e) => {
+      let arrInner = []
+      arrInner.push(e.id)
+      arrInner.push(e.webMonitorId)
+      arrInner.push(e.projectName)
+      arrInner.push(e.projectType)
+      arrInner.push(e.userTag);
+      // Create(Test, {
+      //   id: e.id,
+      //   webMonitorId: e.webMonitorId,
+      //   projectName: e.projectName,
+      //   projectType: e.projectType,
+      //   userTag:e.userTag
+      // }, (a) => {
+      //   console.log('-----a',a);
+
+      // })
+      data.push(arrInner)
+    });
+    console.log('----data', data);
 
   }
 }); 
